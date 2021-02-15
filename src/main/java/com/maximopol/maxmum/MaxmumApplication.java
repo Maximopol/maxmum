8c20a28ae746e7097123c85fca54ce477ce11968
Maksim <maximopolnate@gmail.com>
2021-02-15 21:27:21 +0300

    Initial commit

---
+++ src/main/java/com/maximopol/maxmum/MaxmumApplication.java
@@ -0,0 +1,13 @@
+package com.maximopol.maxmum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxmumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxmumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maximopol/maxmum/MaxmumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximopol.maxmum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxmumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
